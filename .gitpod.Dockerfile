6acb914d54bd02cbc363af5ee5959f39adf33503
Tom Wilson <twilson63@gmail.com>
2021-05-10 18:38:43 +0000

    added ci tests

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,3 +1,5 @@
 FROM gitpod/workspace-full
 
 USER gitpod
+
+RUN curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- test/delete-faq_test.js
+++ test/delete-faq_test.js
@@ -1,18 +1,20 @@
 const test = require('tape')
-const fetchMock = require('fetch-mock')
 const fetch = require('node-fetch')
 const testServer = require('@twilson63/test-server')
 
-globalThis.fetch = fetchMock.sandbox()
-  .delete('https://dev.hyper63.com/data/twilson63/faq-1', {ok: true, mock: true })
-
 const app = require('../server')
+const jwt = require('jsonwebtoken')
+
+const token = jwt.sign({sub: 'test'}, process.env.API_SECRET, {audience: 'https://webcms.hyper.io'})
 
 test('DELETE /api/faqs/:id', async t => {
   t.plan(1)
   const server = testServer(app)
   const result = await ( await fetch(server.url + '/api/faqs/faq-1', {
-    method: 'DELETE'
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
   })).json().catch(err => ({ok: false, message: err.message}))
   console.log(result)
   t.ok(result.ok)
